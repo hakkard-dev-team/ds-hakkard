bfc32b905209cf882a8e3c02890d8a926c98014d
Dev-Osmium <owen@devosmium.xyz>
2019-05-22 15:29:02 +0000

    Add lennar

---
+++ lib/domains/lennar/room/start.c
@@ -0,0 +1,19 @@
+#include <lib.h>
+#include ROOMS_H
+
+inherit LIB_ROOM;
+
+void create() {
+    room::create();
+    SetAmbientLight(30);
+    SetShort("Prancing Dolphin");
+    SetLong("This is a long, low room filled with tables and patrons talking loudly. There is a bar spanning the eastern wall, with stools spaced evenly along it. The bartender paces around behind it, casting glances at some of the more rowdy patrons.");
+    SetItems(([
+        ({ "table", "tables" }) : "These are long wooden tables, that look like they've seen many years of drinking and abuse. Though old, they look sturdy enough to take many more beatings.",
+        ({ "patron", "patrons" }) : "These are smelly, unwashed humans and halflings enjoying themselves enormously.",
+      ]));
+    SetClimate("indoors");
+}
+void init(){
+    ::init();
+}
