45da3a75b4fb7d53a283c6f10a3d67579dd5263d
Dev-Osmium <owen@devosmium.xyz>
2019-05-22 11:34:48 +0000

    Remove driver

--- fluffos-2.23-ds03/fluffos.d
+++
@@ -1,6 +0,0 @@
-provider fluffos {
-  probe lpc__entry(string, string, string);
-  probe lpc__return(string, string, string);
-  probe error(string);
-};
-
--- fluffos-2.23-ds03/packages/async.h
+++
@@ -1,6 +0,0 @@
-#ifndef ASYNC_H_
-#define ASYNC_H_
-
-void check_reqs();
-void complete_all_asyncio();
-#endif /*ASYNC_H_*/
