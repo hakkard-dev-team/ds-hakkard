151204911bacd6660061820a85e85febb4dcf42d
Dev-Osmium <devosmium@devosmium.xyz>
2019-06-07 10:06:42 -0400

    Add training skills to ralf

--- lib/domains/lennarmanor/npc/ralfridley.c
+++ lib/domains/lennarmanor/npc/ralfridley.c
@@ -1,19 +1,22 @@
 #include <lib.h>
 
-inherit LIB_SENTIENT;
+inherit LIB_TRAINER;
 
 static void create() {
     sentient::create();
-    SetKeyName("generic npc");
+    SetKeyName("ralf ridley");
     SetId( ({"ralf", "ralf ridley"}) );
-    SetAdjectives(({"non player"}));
+    SetAdjectives(({"fighter"}));
     SetShort("Ralf Ridley");
     SetLong("This is an older man who wears a faded coat and loose pants complete with feathered hat, he is not a threatening figure. He wears his hair in dreadlocks. His blue eyes lackadaisically let the world flow into them.");
-    SetLevel(1);
+    SetClass("fighter");
+    SetLevel(10);
     SetMelee(1);
     SetRace("human");
     SetGender("male");
+    AddTrainingSkills(({"blade attack", "blade defense", "melee attack", "melee defense"}));
+    SetCustomXP(40);
 }
 void init(){
-    ::init();
+    trainer::init();
 }
